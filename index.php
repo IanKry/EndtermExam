8ffa06d4392724dd9b748ba82e329fab50150fc8
Arian Kryll <71005885+IanKry@users.noreply.github.com>
2020-12-16 22:31:25 +0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -15,21 +15,21 @@
 		<div id="header">
 			<h1> ITL303 </h1>
 			<ul>
-				<li><a href="">Home</a></li>
+				<li><a href="index.php">Home</a></li>
 				<div id="dropdown">
 				<li>Activities</li>
 				<div class = "dropdown-content">
 					<ul>
 						<div class="act">
-						<a href="Activities/Prelim/index.html">CSS</a>
+						<a href="Activities/Prelim/index.php">CSS</a>
 						<br>
-						<a href="Activities/Midterm/index.html">SVG</a>
-						<a href="Activities/Endterm/index.html">CANVAS</a>
+						<a href="Activities/Midterm/index.php">SVG</a>
+						<a href="Activities/Endterm/index.php">CANVAS</a>
 						</div>
 					</ul>	
 				</div>
 				</div>
-				<li><a href="about/index.html">About me</a></li>
+				<li><a href="about/index.php">About me</a></li>
 			</ul>
 		</div>
 		<div id="quotes">
